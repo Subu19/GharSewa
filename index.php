93d203f78cdcb0e133ef7099dfddb588bf8e4f38
subu19 <subuacharya19@gmail.com>
2024-02-21 17:18:02 +0545

    setup php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$routes = [
+    "/" => "src/views/index.php",
+    "/login" => "src/views/login.php"
+];
+
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    include 'views/404.php';
+    http_response_code(404);
+}
